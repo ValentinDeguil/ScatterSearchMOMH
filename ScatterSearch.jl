3c0340d500eb21d33a65ee2da0dd28f8c86ac7dd
valentindeguil <valentin.deguil@gmail.com>
2022-11-11 15:04:52 +0100

    adding beggining if the first grasp (level1)

--- ScatterSearch.jl
+++ ScatterSearch.jl
@@ -30,27 +30,46 @@ function loadInstance(fname)
     return m, n, p, r, concentrators, terminals
 end
 
-function generatePopulation(sizePopulation::Int, concentrators::Matrix{Float32}, p::Int)
-    solutions = Array{Array{Int}}
-    numberConcentrators = size(concentrators,1)
-    setOfConcentrators = []
-    for i in 1:numberConcentrators
-        push!(setOfConcentrators, i)
+function generatePopulation(sizePopulation::Int, concentrators::Matrix{Float32}, Q::Int, numberLevel1::Int, numberLevel2::Int, n::Int, terminals::Matrix{Float32})
+    # here, we'll use several occurences of GRASP to generate individuals of our population
+
+    # first we'll use GRASP to select which level 1 concentrators we open
+    population = Array{Array{Int}}
+    numberConcentrators = numberLevel1 + numberLevel2
+
+    # we determine how many level 1 concentrators will at least be in the solution
+    minimumLevel1::Int = ceil(n/Q)
+    # then we flip a coin, we add a concentrator for each head until we get a tail
+    while(rand()<0.5)
+        minimumLevel1 += 1
+        println("yo")
+    end
+    numberSelectedLevel1 = min(minimumLevel1, numberLevel1)
+
+    # now, we launch the GRASP until we get this number of level 1 concentrators, for the first objective
+    # the first one is selected randomly
+    randomLevel1 = rand(1:numberLevel1)
+    setOfSelectedConcentrators = [randomLevel1]
+    # the other ones are chosen by GRASP with an alpha of 0.7
+    for c1 in 2:numberSelectedLevel1
+
     end
 
-    for i in 1:(Int)(floor(sizePopulation/2))
-        # pick a random facility
-        randConcentrator = rand(1:numberConcentrators)
-        println("randFacility = ", randConcentrator)
-        solution = [randConcentrator]
-        # we add other facility with a metaheuristic based on GRASP
-        CL = copy(setOfConcentrators) # candidate list
-        splice!(CL,randConcentrator)
-        RCL = [] # restricted candidate list
-        for i in 1:(p-1)
+    #for i in 1:numberConcentrators
+    #    push!(setOfConcentrators, i)
+    #end
 
-        end
-    end
+    #for i in 1:(Int)(floor(sizePopulation/2))
+    #    # pick a random facility
+    #    randConcentrator = rand(1:numberConcentrators)
+    #    println("randFacility = ", randConcentrator)
+    #    solution = [randConcentrator]
+    #    # we add other facility with a metaheuristic based on GRASP
+    #    CL = copy(setOfConcentrators) # candidate list
+    #    splice!(CL,randConcentrator)
+    #    RCL = [] # restricted candidate list
+#
+    #end
     for i in (Int)(floor(sizePopulation/2))+1:sizePopulation
 
     end
@@ -58,12 +77,19 @@ end
 
 function main(pathToInstance::String, sizePopulation::Int)
 
+    println("")
+    println("Instance of the problem")
+
     # opening the instance
-    m, n, p, r, concentrators, demands = loadInstance(pathToInstance)
+    m, n, p, r, concentrators, terminals = loadInstance(pathToInstance)
     println("m = ", m, " (# concentrators)")
     println("n = ", n, " (# terminals)")
     println("p = ", p, " (# concentrators to open (level 1 & 2)")
     println("r = ", r)
+
+    # for our constraints we chose the capacitated concentrators
+    # so, we have a maximum of 7 links to each concentrator (level 1 & 2)
+    Q = 7
 
     # here, we shuffle concentrators to avoid bias during the picking
     tempArray = Array{Int}(1:m)
@@ -92,16 +118,23 @@ function main(pathToInstance::String, sizePopulation::Int)
         end
     end
 
+    #TODO genreate random costs between level 1 and level 2 concentrators
+
     # we want to divide the set of concentrators in two levels
     # here, we have 4/5 of level 1 and 1/5 of level 2 concentrators
-    p1 = (Int)(4*p/5)
-    p2 = p - p1
-    println("p1 = ", p1, " (# level 1 concentrators)")
-    println("p2 = ", p2, " (# level 2 concentrators)")
+    ratioLevelOne::Float32 = 4/5;
+    numberLevel1 = (Int)(m*ratioLevelOne)
+    numberLevel2 = m - numberLevel1
+    println("numberLevel1 = ", numberLevel1, " (# level 1 concentrators)")
+    println("numberLevel2 = ", numberLevel2, " (# level 2 concentrators)")
+
+    println("")
+    println("-----------------------------")
+    println("")
 
     # we generate our first population of solution
     # half is good for the first objective the other is good for the second one
-    generatePopulation(sizePopulation, concentrators, p)
+    generatePopulation(sizePopulation, concentrators, Q, numberLevel1, numberLevel2, n, terminals)
 end
 
 main("Instances/verySmall1.txt", 10)
